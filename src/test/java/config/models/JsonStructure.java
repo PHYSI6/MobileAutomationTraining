815d778f9793076f06423cfdfde05a7b0bcbc4a5
PHYSI6 <borisevich.daniel@gmail.com>
2022-09-05 19:03:34 +0300

    Cleaned unuseful part of previous project

--- src/test/java/config/models/JsonStructure.java
+++ src/test/java/config/models/JsonStructure.java
@@ -2,5 +2,4 @@ package config.models;
 
 public class JsonStructure {
     public AppConfig appConfig;
-    public User user;
 }
--- src/test/java/config/models/User.java
+++
@@ -1,9 +0,0 @@
-package config.models;
-
-public class User {
-    public String country;
-    public String phoneCode;
-    public String phoneNumber;
-    public String code;
-    public String pin;
-}
